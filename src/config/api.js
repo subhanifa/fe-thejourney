7465987e0a1792ba036d9aa4bcf94c6425f0445b
unknown <subhanhanifa@gmail.com>
2022-03-25 16:04:45 +0700

    change api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL:
+    process.env.REACT_APP_SERVER_URL ||
+    'https://demo-backend-deploy.herokuapp.com/api/v1' ||
+    'https://localhost:5000/api/v1',
 });
 
 export const setAuthToken = (token) => {
